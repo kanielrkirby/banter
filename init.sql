b11c85e8b4a3150f03bba0898c4d7acdd8a369a9
Piratey7007 <piratey7007@runbox.com>
2023-09-21 21:33:26 -0500

    Working on Prepared Procedures

--- init.sql
+++ init.sql
@@ -1,114 +1,175 @@
--- "User Status" Table
+-------------------------------------------
+-- Lookup Tables --------------------------
+-------------------------------------------
+
+-- "Profile Status" Lookup Table
 CREATE TABLE profile_status (
-  profile_status_name VARCHAR(50) PRIMARY KEY
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(20) NOT NULL UNIQUE
 );
 
-INSERT INTO profile_status (profile_status_name) VALUES 
-('online'), 
-('offline'), 
-('away'), 
-('busy');
+INSERT INTO profile_status (name) VALUES
+  ('deleted'),
+  ('active'),
+  ('offline'),
+  ('busy');
 
--- "User Relation Status" Table
+-- "Profile Relation Status" Lookup Table
 CREATE TABLE profile_relation_status (
-  relation_status_name VARCHAR(50) PRIMARY KEY
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(20) NOT NULL UNIQUE
 );
 
-INSERT INTO profile_relation_status (relation_status_name) VALUES 
-('pending'), 
-('friend'), 
-('blocked'), 
-('ignored'), 
-('requested');
+INSERT INTO profile_relation_status (name) VALUES
+  ('friend'),
+  ('requested'),
+  ('received'),
+  ('blocked'),
+  ('ignored');
 
--- "Room Status" Table
-CREATE TABLE room_status (
-  room_status_name VARCHAR(50) PRIMARY KEY
+-- "Profile Message Status" Lookup Table
+CREATE TABLE profile_message_status (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(20) NOT NULL UNIQUE
 );
 
-INSERT INTO room_status (room_status_name) VALUES 
-('pending'), 
-('member'), 
-('blocked'), 
-('ignored'), 
-('admin'), 
-('owner');
+INSERT INTO profile_message_status (name) VALUES
+  ('read'),
+  ('sent'),
+  ('delivered'),
+  ('deleted');
 
--- "Message Status" Table
-CREATE TABLE message_status (
-  message_status_name VARCHAR(50) PRIMARY KEY
+-- "Profile Room Status" Lookup Table
+CREATE TABLE profile_room_status (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  name VARCHAR(20) NOT NULL UNIQUE
 );
 
-INSERT INTO message_status (message_status_name) VALUES 
-('sent'), 
-('delivered'), 
-('read');
+INSERT INTO profile_room_status (name) VALUES
+  ('owner')
+  ('admin'),
+  ('member'),
+  ('muted'),
+  ('banned'),
+  ('ignored');
 
--- "User "Table
+-------------------------------------------
+-- Primary Tables -------------------------
+-------------------------------------------
+
+-- "Profile" Table
 CREATE TABLE profile (
-  -- this is the username
-  id VARCHAR(100) PRIMARY KEY,
-  hashed_password VARCHAR(1000) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW(),
-  profile_status_name VARCHAR(50) NOT NULL DEFAULT 'offline',
-  FOREIGN KEY (profile_status_name) REFERENCES profile_status (status_name)
-);
-
--- "Room" Table
-CREATE TABLE room (
-  id UUID PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
+  id UUID PRIMARY KEY NOT NULL,
+  username VARCHAR(100) NOT NULL,
+  hashed_password VARCHAR(100) NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT NOW(),
+  updated_at DATETIME NOT NULL DEFAULT NOW(),
+  profile_status_id INT NOT NULL,
+  FOREIGN KEY (profile_status_id) REFERENCES profile_status (id)
 );
 
 -- "Message" Table
 CREATE TABLE message (
-  id UUID PRIMARY KEY,
-  sender_id VARCHAR(100) NOT NULL,
+  id UUID PRIMARY KEY NOT NULL,
+  profile_id UUID NOT NULL,
   room_id UUID NOT NULL,
   body VARCHAR(1000) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW(),
+  created_at DATETIME NOT NULL DEFAULT NOW(),
+  updated_at DATETIME NOT NULL DEFAULT NOW(),
   FOREIGN KEY (profile_id) REFERENCES profile (id),
   FOREIGN KEY (room_id) REFERENCES room (id)
 );
 
--- "User Status on a Room" Table
-CREATE TABLE profile_room (
-  profile_id VARCHAR(100) NOT NULL,
-  room_id UUID NOT NULL,
-  room_status_name VARCHAR(50) NOT NULL DEFAULT 'pending',
-  PRIMARY KEY (profile_id, room_id),
-  FOREIGN KEY (profile_id) REFERENCES profile (id),
-  FOREIGN KEY (room_id) REFERENCES room (id),
-  FOREIGN KEY (room_status_name) REFERENCES room_status (status_name)
+-- "Room" Table
+CREATE TABLE room (
+  id UUID PRIMARY KEY NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  created_at DATETIME NOT NULL DEFAULT NOW(),
+  updated_at DATETIME NOT NULL DEFAULT NOW()
 );
 
--- "User Relation" Table
+-------------------------------------------
+-- Junction Tables ------------------------
+-------------------------------------------
+
+-- "Profile Relation" Table
 CREATE TABLE profile_relation (
-  sender_profile_id VARCHAR(100) NOT NULL,
-  receiver_profile_id VARCHAR(100) NOT NULL,
-  relation_status_name VARCHAR(50) NOT NULL DEFAULT 'pending',
+  requester_profile_id UUID NOT NULL,
+  receiver_profile_id UUID NOT NULL,
+  relation_status_id INT NOT NULL,
   PRIMARY KEY (requester_profile_id, receiver_profile_id),
   FOREIGN KEY (requester_profile_id) REFERENCES profile (id),
   FOREIGN KEY (receiver_profile_id) REFERENCES profile (id),
-  FOREIGN KEY (relation_status_name) REFERENCES profile_relation_status (status_name)
+  FOREIGN KEY (relation_status_id) REFERENCES profile_relation_status (id)
 );
 
--- "User Message" Table
-CREATE TABLE profile_message (
-  sender_profile_id VARCHAR(100) NOT NULL,
-  receiver_profile_id VARCHAR(100) NOT NULL,
+-- "Profile Message Status" Junction Table
+CREATE TABLE profile_message_status (
+  profile_id UUID NOT NULL,
   message_id UUID NOT NULL,
-  message_status_name VARCHAR(50) NOT NULL DEFAULT 'pending',
-  PRIMARY KEY (requester_profile_id, receiver_profile_id, message_id),
-  FOREIGN KEY (requester_profile_id) REFERENCES profile (id),
-  FOREIGN KEY (receiver_profile_id) REFERENCES profile (id),
+  message_status_id INT NOT NULL,
+  PRIMARY KEY (profile_id, message_id),
+  FOREIGN KEY (profile_id) REFERENCES profile (id),
   FOREIGN KEY (message_id) REFERENCES message (id),
-  FOREIGN KEY (message_status_name) REFERENCES message_status (status_name)
+  FOREIGN KEY (message_status_id) REFERENCES message_status (id)
 );
+
+-- "Profile Room" Table
+CREATE TABLE profile_room (
+  profile_id UUID NOT NULL,
+  room_id UUID NOT NULL,
+  profile_room_status_id INT NOT NULL,
+  PRIMARY KEY (profile_id, room_id),
+  FOREIGN KEY (profile_id) REFERENCES profile (id),
+  FOREIGN KEY (room_id) REFERENCES room (id),
+  FOREIGN KEY (room_status_id) REFERENCES profile_room_status (id)
+);
+
+-------------------------------------------
+-- Triggers -------------------------------
+-------------------------------------------
+
+DELIMITER //
+
+--
+CREATE PROCEDURE DeleteEmptyRooms(IN deleted_profile_id UUID)
+BEGIN
+  DELETE r.* FROM room r
+  JOIN profile_room pr
+  ON pr.user_id = deleted_profile_id
+  LEFT JOIN profile p
+  ON p.id = pr.user_id
+  WHERE pr.profile_room_status_id != 3 -- muted
+  AND pr.profile_room_status_id != 4 -- banned
+  AND pr.profile_room_status_id != 5 -- ignored
+  AND p.profile_status_id != 1 -- deleted
+END;
+//
+
+CREATE TRIGGER after_profile_status_update
+AFTER UPDATE ON profile
+FOR EACH ROW
+BEGIN
+    IF NEW.profile_status_id = 4 THEN 
+      -- 4 = deleted
+        CALL DeleteEmptyRooms(NEW.id);
+    END IF;
+END;
+//
+
+
+CREATE TRIGGER after_profile_status_update
+AFTER UPDATE ON profile
+FOR EACH ROW
+BEGIN
+    IF NEW.profile_status_id = 4 THEN
+      -- 4 = deleted
+      DeleteEmptyRooms(NEW.id);
+    END IF;
+END;
+//
+
+
 
 -- "Updated At" Triggers
 CREATE TRIGGER profile_updated_at 
@@ -136,6 +197,14 @@ END;
 //
 
 -- "UUID" Triggers
+CREATE TRIGGER before_profile_insert
+BEFORE INSERT ON profile
+FOR EACH ROW
+  BEGIN
+    SET NEW.id = UUID();
+END;
+//
+
 CREATE TRIGGER before_message_insert
 BEFORE INSERT ON message
 FOR EACH ROW
@@ -152,255 +221,166 @@ BEGIN
 END;
 //
 
+
+-------------------------------------------
+-- Prepared Procedures --------------------
+-------------------------------------------
+
+-- "Create Profile" Procedure
+CREATE PROCEDURE CreateProfile(
+  IN username VARCHAR(100),
+  IN hashed_password VARCHAR(1000),
+  IN profile_status_id INT
+)
+BEGIN
+  INSERT INTO profile (username, hashed_password, profile_status_id)
+  VALUES (username, hashed_password, profile_status_id);
+END;
+//
+
+-- "Create Message" Procedure
+CREATE PROCEDURE CreateMessage(
+  IN sender_id UUID,
+  IN room_id UUID,
+  IN body VARCHAR(1000)
+)
+BEGIN
+  DECLARE message_id UUID;
+  INSERT INTO message (profile_id, room_id, body)
+  VALUES (sender_id, room_id, body);
+  SET message_id = LAST_INSERT_ID();
+  INSERT INTO profile_message_status (profile_id, message_id, message_status_id)
+  -- 1 = sent
+  VALUES (profile_id, message_id, 1);
+END;
+//
+
+-- "Create Room" Procedure
+CREATE PROCEDURE CreateRoom(
+  IN name VARCHAR(100),
+  IN profile_id VARCHAR(100)
+)
+BEGIN
+  DECLARE room_id UUID;
+  INSERT INTO room (name) VALUES (name);
+  SET room_id = LAST_INSERT_ID();
+  INSERT INTO profile_room (profile_id, room_id, room_status_name)
+  VALUES (profile_id, room_id, 'admin');
+END;
+//
+
+-- "Create Profile Relation" Procedure
+CREATE PROCEDURE CreateProfileRelation(
+  IN requester_profile_id VARCHAR(100),
+  IN receiver_profile_id VARCHAR(100),
+  IN relation_status_id INT
+)
+BEGIN
+  INSERT INTO profile_relation (requester_profile_id, receiver_profile_id, relation_status_id)
+  VALUES (requester_profile_id, receiver_profile_id, relation_status_id);
+END;
+//
+
+-- "Update Profile Relation" Procedure
+CREATE PROCEDURE UpdateProfileRelation(
+  IN requester_profile_id VARCHAR(100),
+  IN receiver_profile_id VARCHAR(100),
+  IN relation_status_id INT
+)
+BEGIN
+  UPDATE profile_relation
+  SET relation_status_id = relation_status_id
+  WHERE requester_profile_id = requester_profile_id
+  AND receiver_profile_id = receiver_profile_id;
+END;
+//
+
+-- "Send Friend Request" Procedure
+CREATE PROCEDURE SendFriendRequest(
+  IN requester_profile_id VARCHAR(100),
+  IN receiver_profile_id VARCHAR(100)
+)
+BEGIN
+  CALL CreateProfileRelation(requester_profile_id, receiver_profile_id, 1);
+  CALL CreateProfileRelation(receiver_profile_id, requester_profile_id, 2);
+  -- 1 = requested
+  -- 2 = received
+END;
+//
+
+-- "Accept Friend Request" Procedure
+CREATE PROCEDURE AcceptFriendRequest(
+  IN requester_profile_id VARCHAR(100),
+  IN receiver_profile_id VARCHAR(100)
+)
+BEGIN
+  CALL UpdateProfileRelation(requester_profile_id, receiver_profile_id, 1);
+  CALL UpdateProfileRelation(receiver_profile_id, requester_profile_id, 1);
+  -- 1 = friend
+END;
+//
+
+-- "Block Profile" Procedure
+CREATE PROCEDURE BlockProfile(
+  IN requester_profile_id VARCHAR(100),
+  IN receiver_profile_id VARCHAR(100)
+)
+BEGIN
+  CALL UpdateProfileRelation(requester_profile_id, receiver_profile_id, 4);
+  -- 4 = blocked
+END;
+//
+
+-- "Ignore Profile" Procedure
+CREATE PROCEDURE IgnoreProfile(
+  IN requester_profile_id VARCHAR(100),
+  IN receiver_profile_id VARCHAR(100)
+)
+BEGIN
+  CALL UpdateProfileRelation(requester_profile_id, receiver_profile_id, 5);
+  -- 5 = ignored
+END;
+//
+
+-- "Delete Profile" Procedure
+CREATE PROCEDURE DeleteProfile(
+  IN profile_id VARCHAR(100)
+)
+BEGIN
+  UPDATE profile
+  SET profile_status_id = 1
+  WHERE id = profile_id;
+  -- 1 = deleted
+END;
+//
+
+-- "Delete Message" Procedure
+CREATE PROCEDURE DeleteMessage(
+  IN profile_id VARCHAR(100),
+  IN message_id UUID
+)
+BEGIN
+  UPDATE profile_message_status
+  SET message_status_id = 4
+  WHERE profile_id = profile_id
+  AND message_id = message_id;
+  -- 4 = deleted
+END;
+//
+
+-- "Delete Room" Procedure
+CREATE PROCEDURE DeleteRoom(
+  IN profile_id VARCHAR(100),
+  IN room_id UUID
+)
+BEGIN
+  UPDATE profile_room
+  SET room_status_id = 4
+  WHERE profile_id = profile_id
+  AND room_id = room_id;
+  -- 4 = ignored
+END;
+//
+
+
 DELIMITER ;
-
-INSERT INTO profile (username, password, profile_status_name) VALUES 
-  ('John', 'f29afg7p24', 'online'),
-  ('Jane', 'f29afg7p24', 'offline'),
-  ('Bob', 'f29afg7p24', 'away'),
-  ('Alice', 'f29afg7p24', 'busy'),
-  ('Eve', 'f29afg7p24', 'online'),
-  ('Mallory', 'f29afg7p24', 'offline'),
-  ('Trent', 'f29afg7p24', 'away'),
-  ('Carol', 'f29afg7p24', 'busy'),
-  ('Dave', 'f29afg7p24', 'online'),
-  ('Oscar', 'f29afg7p24', 'offline');
-
-INSERT INTO room (id, name) VALUES
-  (UUID(), 'General'),
-  (UUID(), 'Random'),
-  (UUID(), 'Music'),
-  (UUID(), 'Programming'),
-  (UUID(), 'Gaming');
-
-INSERT INTO profile_room (profile_id, room_id, room_status_name) VALUES
-(
-  (SELECT id FROM profile WHERE username = 'John'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'admin'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'member'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Bob'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'muted'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Alice'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'owner'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Eve'),
-  (SELECT id FROM room WHERE name = 'Gaming'),
-  'banned'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Mallory'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'ignored'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Trent'),
-  (SELECT id FROM room WHERE name = 'Random'),
-  'admin'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Carol'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'member'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Dave'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'muted'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Oscar'),
-  (SELECT id FROM room WHERE name = 'Gaming'),
-  'owner'
-);
-
-INSERT INTO profile_relation (requester_profile_id, receiver_profile_id, relation_status_name) VALUES
-(
-  (SELECT id FROM profile WHERE username = 'John'),
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  'friend'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  (SELECT id FROM profile WHERE username = 'Bob'),
-  'requested'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Bob'),
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  'pending'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Alice'),
-  (SELECT id FROM profile WHERE username = 'Eve'),
-  'friend'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Eve'),
-  (SELECT id FROM profile WHERE username = 'Mallory'),
-  'blocked'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Mallory'),
-  (SELECT id FROM profile WHERE username = 'Trent'),
-  'friend'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Trent'),
-  (SELECT id FROM profile WHERE username = 'Carol'),
-  'requested'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Carol'),
-  (SELECT id FROM profile WHERE username = 'Dave'),
-  'ignored'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Dave'),
-  (SELECT id FROM profile WHERE username = 'Oscar'),
-  'friend'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Oscar'),
-  (SELECT id FROM profile WHERE username = 'John'),
-  'blocked'
-);
-
-INSERT INTO message (profile_id, room_id, body) VALUES
-(
-  (SELECT id FROM profile WHERE username = 'John'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'Hello everyone in the General room!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'Hi John! How are you?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Alice'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'Anyone familiar with SQL triggers?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Dave'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'Yes, Alice. What do you need help with?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Bob'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'Any recommendations for good rock songs?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Eve'),
-  (SELECT id FROM room WHERE name = 'Gaming'),
-  'Has anyone tried the new game "Endless Horizon"?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Oscar'),
-  (SELECT id FROM room WHERE name = 'Gaming'),
-  'Yes, Eve. I played it last night. It''s awesome!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Carol'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'I love classic jazz. Any recommendations?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Mallory'),
-  (SELECT id FROM room WHERE name = 'Random'),
-  'Did anyone watch the lunar eclipse yesterday?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Trent'),
-  (SELECT id FROM room WHERE name = 'Random'),
-  'I missed it! Any good pictures?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'John'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'By the way, there''s a team meeting tomorrow at 10 AM.'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Bob'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'Check out "Bohemian Rhapsody" by Queen. A classic!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Alice'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'I figured out the SQL trigger issue. Thanks, Dave!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'Thanks for the heads up, John.'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Dave'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'No problem, Alice. Always here to help!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Eve'),
-  (SELECT id FROM room WHERE name = 'Gaming'),
-  'Oscar, let''s team up for a multiplayer session tonight.'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Oscar'),
-  (SELECT id FROM room WHERE name = 'Gaming'),
-  'Sounds like a plan, Eve!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Mallory'),
-  (SELECT id FROM room WHERE name = 'Random'),
-  'I captured some amazing shots of the lunar eclipse. Will share them soon.'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Trent'),
-  (SELECT id FROM room WHERE name = 'Random'),
-  'Looking forward to it, Mallory!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Carol'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'Thanks for the jazz recommendations. Loving it!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Bob'),
-  (SELECT id FROM room WHERE name = 'Music'),
-  'Glad you liked it, Carol!'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Jane'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'Can we postpone the team meeting to 11 AM?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'John'),
-  (SELECT id FROM room WHERE name = 'General'),
-  'Sure, Jane. I''ll make the change.'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Alice'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'Anyone attending the tech conference next week?'
-),
-(
-  (SELECT id FROM profile WHERE username = 'Dave'),
-  (SELECT id FROM room WHERE name = 'Programming'),
-  'I am, Alice. Let''s catch up there!'
-);
-
