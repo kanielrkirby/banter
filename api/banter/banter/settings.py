6c7aff1bd414262270a820151d08d740f8547460
Piratey7007 <piratey7007@runbox.com>
2023-09-22 13:29:16 -0500

    Updated docker for SQL

--- api/banter/banter/settings.py
+++ api/banter/banter/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="../../.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +84,12 @@ WSGI_APPLICATION = 'banter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'banter',
+         'USER': os.getenv('MYSQL_USER'),
+         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+         'HOST': os.getenv('MYSQL_HOST'),
+         'PORT': os.getenv('MYSQL_PORT'),
     }
 }
 
