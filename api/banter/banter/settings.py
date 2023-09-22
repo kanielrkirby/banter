b3b70079f89202b53e8c49ee1e47e28ddc2028de
Piratey7007 <piratey7007@runbox.com>
2023-09-22 12:47:35 -0500

    Updated migrations

--- api/banter/banter/settings.py
+++ api/banter/banter/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'user',
-    'message',
+    
+    'user_profile',
     'room',
 ]
 
