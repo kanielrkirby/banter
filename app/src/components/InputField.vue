5d4d6fcc9bf3ee7afa7aaa218d17d95b37703851
Piratey7007 <piratey7007@runbox.com>
2023-09-30 11:36:31 -0500

    Added InputField

---
+++ app/src/components/InputField.vue
@@ -0,0 +1,61 @@
+<template>
+    <label :for="id">
+      <span>
+        {{ label }}
+      </span>
+    <input
+      :type="type"
+      :name="name"
+      :id="id"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :placeholder="placeholder"
+      :maxlength="maxlength"
+      :minlength="minlength"
+      :required="required"
+    />
+    </label>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+export const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  id: {
+    type: String,
+    required: true,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  maxlength: {
+    type: Number,
+    default: 100,
+  },
+  minlength: {
+    type: Number,
+    default: 0,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
