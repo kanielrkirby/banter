78b70d500d956b83f0cf573f2ad07aa6fac8083b
Piratey7007 <piratey7007@runbox.com>
2023-09-30 14:17:08 -0500

    Added colors

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['src/**/*.{html,ts,tsx,js,jsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#12141A',
+        secondary: '#1E2435',
+        accent: '#F2DC83',
+        'accent-dark': '#CF9549',
+        'secondary-accent': '#845CEE',
+        'secondary-accent-dark': '#6F11E5',
+      },
+    },
   },
   plugins: [],
 }
