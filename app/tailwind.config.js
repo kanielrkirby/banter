c797df61d1b6f54b74da75db02dc4289eeb01615
Piratey7007 <piratey7007@runbox.com>
2023-09-30 14:58:25 -0500

    Added fonts

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ['index.html', 'src/**/*.{html,ts,tsx,js,jsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        body: ['Lato', 'sans-serif'],
+        heading: ['Rye', 'serif'],
+        'heading-secondary': ['Inknut Antiqua', 'serif'],
+      },
       colors: {
         primary: '#12141A',
         secondary: '#1E2435',
@@ -13,6 +18,17 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents, addBase, addUtilities }) {
+      addBase({
+        'h1,h2,h3,h4,h5,h6': {
+          fontFamily: 'Rye, serif',
+        },
+        '*,*::before,*::after': {
+          fontFamily: 'Lato, sans-serif',
+        },
+      })
+    }
+  ],
 }
 
