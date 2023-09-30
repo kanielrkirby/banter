d9637f77f10c254ccf9feb228a7efeb46e55d204
Piratey7007 <piratey7007@runbox.com>
2023-09-29 19:51:14 -0500

    Auth works

--- api/banter/banter/urls.py
+++ api/banter/banter/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/profile/', include('user_profile.urls')),
-    path('api/room/', include('room.urls')),
-    path('api/auth/', include('user_auth.urls')),
+    path('profile/', include('user_profile.urls')),
+    path('room/', include('room.urls')),
+    path('auth/', include('user_auth.urls')),
 ]
--- api/banter/user_profile/models.py
+++ api/banter/user_profile/models.py
@@ -8,6 +8,35 @@ from .enums import ProfileRelationStatusEnum, ProfileStatusEnum
 from room.enums import RoomProfileStatusEnum
 from room.models import Room
 
+class ProfileManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        """
+        Create and return a regular user with the given username and password.
+        """
+        if not username:
+            raise ValueError('The Username field must be set')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        """
+        Create and return a superuser with the given username and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
+
 class Profile(AbstractBaseUser, PermissionsMixin):
     """
     Represents a user profile.
@@ -19,6 +48,7 @@ class Profile(AbstractBaseUser, PermissionsMixin):
         updated_at: the date and time the profile was last updated
         status: the status of the profile [active, offline, busy, deleted]
     """
+    objects = ProfileManager()
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(_('password'), max_length=128, null=True)
