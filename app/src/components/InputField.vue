dde6df63df0bd517a78dfd56042a03bfcd6d8fb2
Piratey7007 <piratey7007@runbox.com>
2023-11-08 13:32:32 -0600

    Updated the input to have more natural animations

--- app/src/components/InputField.vue
+++ app/src/components/InputField.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-center items-center relative">
-    <label class="absolute top-0 left-0 text-gray-400 transition-all duration-300 h-fit my-auto"
-      :class="[ selected ? 'opacity-100 text-xs absolute left-1 top-2 bottom-full' : 'opacity-50 text-md top-0 bottom-0 left-3 absolute' ]" :for="id">
+  <div class="flex justify-center items-center relative transition-all duration-200 hover:scale-105 group">
+    <label class="absolute top-0 left-0 text-white transition-all duration-300 h-fit my-auto group-hover:opacity-60"
+      :class="[ selected ? 'opacity-80 text-xs absolute left-1 top-2 bottom-full group-hover:!opacity-90' : 'opacity-40 text-md top-0 bottom-0 left-3 absolute' ]" :for="id">
       {{ label }}
     </label>
     <input :type="type" :name="name" :id="id" :value="modelValue"
