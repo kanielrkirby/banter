991c576b4ba32878497d8e2e0361620f27a5d9cb
Piratey7007 <piratey7007@runbox.com>
2023-11-08 15:02:12 -0600

    Finished styling the Sidebar

--- app/src/components/Sidebar.vue
+++ app/src/components/Sidebar.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="flex flex-col items-center p-2 justify-between">
     <div class="flex flex-col items-center gap-2">
-      <Logo />
-      <hr />
+      <Logo extraClass="p-2 opacity-20" />
+      <hr class="bg-black h-0.5 w-2/3 rounded-full opacity-20" />
       <a href="/" class="bg-opacity-0 rounded-md p-2 bg-black transition-all duration-100 hover:bg-opacity-10 group">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" class="w-8 h-8 box-content transition-all duration-150 group-hover:scale-110">
           <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -21,7 +21,7 @@
       <UserIcon />
     </div>
     <div class="flex flex-col items-center gap-2">
-      <hr />
+      <hr class="bg-black h-0.5 w-2/3 rounded-full opacity-20" />
       <a href="/logout" class="bg-opacity-0 rounded-md p-2 bg-black transition-all duration-100 hover:bg-opacity-10 group">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000" class="w-8 h-8 box-content transition-all duration-150 group-hover:scale-110">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
@@ -33,4 +33,5 @@
 
 <script lang="ts" setup>
 import Logo from "@/components/Logo.vue";
+//import UserIcon from "@/components/UserIcon.vue";
 </script>
