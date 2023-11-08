bc1f98f704408687c2a3bebbfec8b8dc4cbbf900
Piratey7007 <piratey7007@runbox.com>
2023-11-08 11:21:31 -0600

    Added tailwind colors from figma file

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -9,21 +9,87 @@ module.exports = {
         'heading-secondary': ['Inknut Antiqua', 'serif'],
       },
       colors: {
-        primary: '#1E2435',
-        'primary-faded': '#262E44',
-        'primary-light': '#234267',
-        'primary-dark': '#12141A',
-        secondary: '#234B54',
-        'secondary-dark': '#0C2E36',
-        accent: '#F2DC83',
-        'accent-dark': '#CF9549',
-        'secondary-accent': '#845CEE',
-        'secondary-accent-dark': '#6F11E5',
+        fg: "#000000",
+        bg: "#f0f0f0",
+        card: {
+          50: "#f8ece6",
+          100: "#ffcdb2",
+          300: "#ffb4a2",
+          500: "#e5989b",
+          700: "#b5838d",
+          900: "#6d6875",
+          950: "#32343c"
+        },
+        primary: {
+          50: "#fffbe5",
+          100: "#fff7cc",
+          200: "#fff099",
+          300: "#ffe866",
+          400: "#ffe033",
+          500: "#ffe347",
+          600: "#ccad00",
+          700: "#998200",
+          800: "#665700",
+          900: "#332b00",
+          950: "#1a1600"
+        },
+        error: {
+          50: "#fde7e7",
+          100: "#fccfcf",
+          200: "#f8a0a0",
+          300: "#f57070",
+          400: "#f14141",
+          500: "#f03333",
+          600: "#be0e0e",
+          700: "#8f0a0a",
+          800: "#5f0707",
+          900: "#300303",
+          950: "#180202"
+        },
+        success: {
+          50: "#eafce8",
+          100: "#d5f9d2",
+          200: "#abf4a4",
+          300: "#81ee77",
+          400: "#57e949",
+          500: "#44e635",
+          600: "#24b616",
+          700: "#1b8811",
+          800: "#125b0b",
+          900: "#092d06",
+          950: "#041703"
+        },
+        info: {
+          50: "#e7f4fe",
+          100: "#cfe9fc",
+          200: "#9ed4fa",
+          300: "#6ebef7",
+          400: "#3ea8f4",
+          500: "#259df3",
+          600: "#0b75c1",
+          700: "#085891",
+          800: "#053b61",
+          900: "#031d30",
+          950: "#010f18"
+        },
+        G: {
+          50: "#f2f2f2",
+          100: "#e6e6e6",
+          200: "#cccccc",
+          300: "#b3b3b3",
+          400: "#999999",
+          500: "#949494",
+          600: "#666666",
+          700: "#4d4d4d",
+          800: "#333333",
+          900: "#1a1a1a",
+          950: "#0d0d0d"
+        },
       },
     },
   },
   plugins: [
-    function ({ addComponents, addBase, addUtilities, matchComponents }) {
+    function({ addComponents, addBase, addUtilities, matchComponents }) {
       addBase({
         'h1,h2,h3,h4,h5,h6': {
           fontFamily: 'Rye, serif',
