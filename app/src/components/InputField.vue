1c5cb7b89b5a144450437296de2ea56e5203be52
Piratey7007 <piratey7007@runbox.com>
2023-11-08 12:07:10 -0600

    Fancy input animation

--- app/src/components/InputField.vue
+++ app/src/components/InputField.vue
@@ -1,17 +1,21 @@
 <template>
-  <label :for="id" class="w-full">
-    <span>
+  <div class="flex justify-center items-center relative">
+    <label class="absolute top-0 left-0 text-gray-400 transition-all duration-300 h-fit my-auto"
+      :class="[ selected ? 'opacity-100 text-xs absolute left-1 top-2 bottom-full' : 'opacity-50 text-md top-0 bottom-0 left-3 absolute' ]" :for="id">
       {{ label }}
-    </span>
-    <input :type="type" :name="name" :id="id" :value="modelValue" :placeholder="placeholder ?? label"
+    </label>
+    <input :type="type" :name="name" :id="id" :value="modelValue"
       :maxlength="maxlength" :minlength="minlength" :required="required"
+      @focus="selected = true" @blur="selected = false"
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
-      class="bg-card-950 px-2 py-2.5 rounded-sm font-medium w-full" />
-  </label>
+      class="bg-card-950 px-2 py-3 rounded-sm font-medium w-full" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
+
+const selected = ref(false);
 
 defineProps<{
   modelValue: string;
